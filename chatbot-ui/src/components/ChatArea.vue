bdd5dd25f05eec538a6c580e48f63c7a257fee37
Lok Leoi <lyulelok@gmail.com>
2025-06-12 22:58:09 +0800

    update chat ui

--- chatbot-ui/src/components/ChatArea.vue
+++ chatbot-ui/src/components/ChatArea.vue
@@ -7,10 +7,19 @@
 	const chatHistory = ref([])
 	const userInput = ref('')
 
+	const generateRandomResponse = () => {
+		const randomIndex = Math.floor(Math.random() * mockResponse.length)
+		return mockResponse[randomIndex]
+	}
+
+	const randomRes = ref(generateRandomResponse())
+
 	const handleMessageSubmit = () => {
-		chatHistory.value.push(userInput.value)
-		console.log('current chat history', chatHistory.value)
+		chatHistory.value.push({ role: 'user', value: userInput.value })
 		userInput.value = ''
+		randomRes.value = generateRandomResponse()
+		chatHistory.value.push(randomRes.value)
+		console.log('current chat history', chatHistory.value)
 	}
 </script>
 
@@ -21,7 +30,13 @@
 			class="flex justify-center items-center">
 			<p class="text-2xl">Hello. Let's start chatting!</p>
 		</div>
-		<div v-else class="flex"></div>
+		<div v-else class="flex flex-col">
+			<div v-for="chat in chatHistory">
+				<p class="border-2 border-amber-300">
+					{{ chat.role }}: {{ chat.value }}
+				</p>
+			</div>
+		</div>
 		<Input
 			class="mt-auto"
 			v-model="userInput"
