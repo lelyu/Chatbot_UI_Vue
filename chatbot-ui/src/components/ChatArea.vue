0afa72fa0e37f0d57335fef5752494ad64ac3813
Lok Leoi <lyulelok@gmail.com>
2025-06-12 22:07:17 +0800

    UI work

--- chatbot-ui/src/components/ChatArea.vue
+++ chatbot-ui/src/components/ChatArea.vue
@@ -1,10 +1,21 @@
 <script setup>
 	import { ref } from 'vue'
+	import Input from './Input.vue'
+	import mockData from '../assets/mockData.json'
+	import mockResponse from '../assets/mockResponse.json'
+
+	const chatHistory = ref([])
 </script>
 
 <template>
-	<div class="w-full h-full border-4 border-amber-400 p-2">
-		<p>Chat Area</p>
+	<div class="w-full h-full border-4 border-amber-400 p-2 flex flex-col">
+		<div
+			v-if="chatHistory.length === 0"
+			class="flex justify-center items-center">
+			<p class="text-2xl">Hello. Let's start chatting!</p>
+		</div>
+		<div v-else class="flex"></div>
+		<Input class="mt-auto" />
 	</div>
 </template>
 
--- chatbot-ui/src/components/ChatUI.vue
+++ chatbot-ui/src/components/ChatUI.vue
@@ -8,7 +8,6 @@
 	<div class="w-full h-full border-2 border-red-400 flex flex-col p-4 gap-4">
 		<h1 class="text-xl font-bold shrink-0">Chat UI</h1>
 		<ChatArea />
-		<Input />
 	</div>
 </template>
 
