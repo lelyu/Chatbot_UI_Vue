e3ab34da031919aebaf0783973c2dfa390864caa
Lok Leoi <lyulelok@gmail.com>
2025-06-12 17:22:36 +0800

    project init

---
+++ chatbot-ui/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+	import ChatUI from './components/ChatUI.vue'
+</script>
+
+<template>
+	<h1>Hello World</h1>
+	<ChatUI />
+</template>
+
+<style scoped></style>
---
+++ chatbot-ui/src/components/ChatUI.vue
@@ -0,0 +1,11 @@
+<script setup>
+	import { ref } from 'vue'
+	import ChatArea from './ChatArea.vue'
+</script>
+
+<template>
+	<h1>Chat UI</h1>
+	<ChatArea />
+</template>
+
+<style scoped></style>
